8a1281d7e331c465f0f7bedfadccb5e4295d91f9
Ruwanthie <dasperera2@gmail.com>
2019-09-09 22:44:25 +0530

    JPA / Hibernate

---
+++ springdata-jpa-example/src/main/java/mainModule/SpringdataJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package mainModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataJpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataJpaExampleApplication.class, args);
+	}
+
+}
